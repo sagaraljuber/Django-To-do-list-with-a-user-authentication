ba158591f6165214920531660945c38822e7d8f9
Juber Sagaral <sagaraljuber7@gmail.com>
2021-10-28 18:05:35 +0800

    CreateView added

--- base/views.py
+++ base/views.py
@@ -1,13 +1,27 @@
 from typing import List
 from django.shortcuts import render
 from django .views.generic.list import ListView
+from django .views.generic.detail import DetailView
+from django .views.generic.edit import CreateView
 
-from base.models import Task
+from django.urls import reverse_lazy
+ 
 from .models import Task
+
+
 
 class TaskList(ListView):
     model = Task
     context_object_name = 'tasks'
 
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'base/task.html'
 
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+    template_name ="base/task_form.html"
 
