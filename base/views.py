256ccfae8a2cc38c84c0831102394a06eb44e219
Juber Sagaral <sagaraljuber7@gmail.com>
2021-10-30 13:46:52 +0800

    Added Register

--- base/views.py
+++ base/views.py
@@ -1,14 +1,17 @@
 from typing import List
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from django .views.generic.list import ListView
 from django .views.generic.detail import DetailView
-from django .views.generic.edit import CreateView, UpdateView, DeleteView
+from django .views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.views import LogoutView
 from django.urls import reverse_lazy
 
 from django.contrib.auth.mixins import LoginRequiredMixin
- 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
 from .models import Task
 
 
@@ -19,6 +22,14 @@ class CustomLoginView(LoginView):
 
     def get_success_url(self):
         return reverse_lazy('tasks')
+
+class RegisterPage(FormView):
+    template_name = 'base/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+
+
 
 
 
@@ -43,9 +54,19 @@ class TaskDetail(LoginRequiredMixin, DetailView):
 
 class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
     template_name ="base/task_form.html"
+
+    def form_invalid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreate, self).form_valid(form)
+
+
+
+
+
+
 
 class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
