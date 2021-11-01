bccf69a64a737d5b3f62082ae19c13a2f1889076
Juber Sagaral <sagaraljuber7@gmail.com>
2021-11-01 22:47:44 +0800

    Added seach input

--- base/views.py
+++ base/views.py
@@ -1,6 +1,6 @@
 from typing import List
 from django.contrib.auth.models import User
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django .views.generic.list import ListView
 from django .views.generic.detail import DetailView
 from django .views.generic.edit import CreateView, UpdateView, DeleteView, FormView
@@ -29,6 +29,17 @@ class RegisterPage(FormView):
     redirect_authenticated_user = True
     success_url = reverse_lazy('tasks')
 
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+    
+    def get(self,  *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+
+        return super(RegisterPage, self).get(*args, **kwargs)
 
 
 
@@ -44,7 +55,14 @@ class TaskList(LoginRequiredMixin, ListView):
         context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
         context['count'] = context['tasks'].filter(complete=False).count()
+
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(title__icontains=search_input)
         return context
+
+        context['search_input'] = search_input
+
         
 
 class TaskDetail(LoginRequiredMixin, DetailView):
